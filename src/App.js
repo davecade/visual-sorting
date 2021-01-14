d6355837ecf4d9a15d16976d1796ba97529b1159
Dave <dave.cadelina@outlook.com>
2021-01-15 02:32:23 +1100

    can no longer click while sort is running

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Graph from './components/graph/graph.component'
 class App extends Component {
   constructor(props) {
     super(props)
-
+    // [1, 5, 16, 6, 10, 15, 13, 7, 11, 14, 5, 4 ,9, 8, 6 ,4, 3, 3, 7]
     this.state = {
-        list: [1, 5, 16, 6, 10, 15, 13, 7, 11, 14, 5, 4 ,9, 8, 6 ,4, 3, 3, 7],
+        list: [1,2,3,4,5,6,7,2,9,10],
         pointer: 0,
         sortRunning: false
     }
@@ -18,12 +18,14 @@ class App extends Component {
       let array = this.state.list
       let count = array.length-1
       let i = this.state.pointer;
+      let finished = true
       
       let started = setInterval(()=> {
         if(array[i]>array[i+1]) {
             let temp = array[i]
             array[i] = array[i+1]
             array[i+1] = temp
+            finished = false;
         }
 
         if(i===count) {
@@ -35,7 +37,7 @@ class App extends Component {
 
         this.setState({list: array, pointer: i, sortRunning: true})
 
-        if(count<0) {
+        if(count<0 || (finished===true && i===count)) {
           clearInterval(started)
           this.setState({sortRunning: false})
         }
