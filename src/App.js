b365be3ade9e7bcae4b562db2e9db496b41aa025
Dave <dave.cadelina@outlook.com>
2021-01-16 02:05:11 +1100

    added insertion sort method

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import Graph from './components/graph/graph.component'
 class App extends Component {
   constructor(props) {
     super(props)
-    // [1, 5, 16, 6, 10, 15, 13, 7, 11, 14, 5, 4 ,9, 8, 6 ,4, 3, 3, 7]
-    // [1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,20,19]
+
     this.state = {
         list: [1, 5, 16, 6, 10, 15, 13, 7, 11, 14, 5, 4 ,9, 8, 6 ,4, 3, 3, 7],
         pointer: 0,
         sortRunning: false
     }
+  }
+
+  reset = () => {
+    let originalData = [1, 5, 16, 6, 10, 15, 13, 7, 11, 14, 5, 4 ,9, 8, 6 ,4, 3, 3, 7]
+    this.setState({list: originalData})
   }
 
   bubbleSort = () => {
@@ -54,6 +58,47 @@ class App extends Component {
     }
   }
 
+  insertionSort = () => {
+    if(!this.state.sortRunning) {
+      let array = this.state.list
+      let i = 0
+      let currentIdx = 0
+      let timeToIterate = true
+
+      let started = setInterval(()=> {
+
+        if(array.length>1) {
+
+          if(timeToIterate) {
+            i++;
+            currentIdx=i;
+            timeToIterate=false
+          }
+
+          if(currentIdx > 0 && (array[currentIdx] < array[currentIdx-1])) {
+            let temp = array[currentIdx]
+            array[currentIdx] = array[currentIdx-1]
+            array[currentIdx-1] = temp
+            currentIdx--;
+          } else {
+            timeToIterate = true
+          }
+        }
+
+        this.setState({list: array, pointer: currentIdx, sortRunning: true})
+
+        if(array.length===1 || i===array.length) {
+          console.log("CLEARED")
+          clearInterval(started)
+          this.setState({sortRunning: false})
+        }
+
+      }, 50)
+
+    }
+
+  }
+
 
   render() {
 
@@ -61,7 +106,9 @@ class App extends Component {
       <div className="App">
         <h1 className="title">SORTING APP</h1>
         <Graph list={this.state.list} pointer={this.state.pointer} sortRunning={this.state.sortRunning} />
-        <button className="sort-button" onClick={this.bubbleSort}>SORT</button>
+        <button className="sort-button bubble" onClick={this.bubbleSort}>BUBBLE SORT</button>
+        <button className="sort-button insertion" onClick={this.insertionSort}>INSERTION SORT</button>
+        <button className="sort-button reset" onClick={this.reset}>RESET</button>
       </div>
     )
   }
