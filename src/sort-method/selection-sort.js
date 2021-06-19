5d40976074409900399f21c44249ecf1e5e79b7b
Dave <dave.cadelina@outlook.com>
2021-06-19 10:42:30 +1000

    changed sound files

--- src/sort-method/selection-sort.js
+++ src/sort-method/selection-sort.js
@@ -1,6 +1,6 @@
 import {React, Component } from 'react';
 import { connect } from 'react-redux';
-import { sound, playSortingSound, playFinishedSound } from '../data/sound'
+import { sound, playSortingSound, playFinishedSound } from '../sound-utils/sound'
 import { 
     generateGraph,
     toggleSortRunning,
