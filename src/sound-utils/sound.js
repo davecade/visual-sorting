6aa9a794dbeb4be6259ef83ad4269394cefb48d7
Dave <dave.cadelina@outlook.com>
2021-08-09 21:27:17 +1000

    added new sound

--- src/sound-utils/sound.js
+++ src/sound-utils/sound.js
@@ -1,5 +1,5 @@
 export const sound = {
-    sorting: new Audio('https://raw.githubusercontent.com/davecade/sort-algorithm-display/master/src/media/laser.mp3'),
+    sorting: new Audio('https://raw.githubusercontent.com/davecade/sort-algorithm-display/master/src/media/laser2.mp3'),
     finished: new Audio('https://raw.githubusercontent.com/davecade/sort-algorithm-display/master/src/media/ding.mp3')
 }
 
